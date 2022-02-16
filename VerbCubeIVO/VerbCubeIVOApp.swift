3e977149d98f01adc845464df1b39eba15f95f49
ChuckDiggins <cfdiggins@yahoo.com>
2022-02-16 15:10:23 -0700

    Initial Commit

---
+++ VerbCubeIVO/VerbCubeIVOApp.swift
@@ -0,0 +1,17 @@
+//
+//  VerbCubeIVOApp.swift
+//  VerbCubeIVO
+//
+//  Created by Charles Diggins on 2/16/22.
+//
+
+import SwiftUI
+
+@main
+struct VerbCubeIVOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
